a053ddebad6abc94539aa27e4a9d566343fa3b16
Arfin Hosain <developerarfin@gmail.com>
2021-10-24 12:46:36 +0600

    done

--- app/src/main/java/com/example/bohubrihiwithjetpack/MainActivity.kt
+++ app/src/main/java/com/example/bohubrihiwithjetpack/MainActivity.kt
@@ -3,43 +3,46 @@ package com.example.bohubrihiwithjetpack
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import com.example.bohubrihiwithjetpack.retrofit.RetrofitClient
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Response
+import java.lang.Exception
+import javax.security.auth.callback.Callback
 
 class MainActivity : ComponentActivity() {
-
-    private val apiKey = "rNCnpnQge4Il62hiTcLVX6FAr8IPG7oW"
+    private val weatherAPI = "rNCnpnQge4Il62hiTcLVX6FAr8IPG7oW"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.dddd)
-        getRegionByHttp()
+       setContent {
+           composable()
+
+       }
     }
 
-    private fun getRegionByHttp() {
-        RetrofitClient.retrofitInstance?.getApi()?.getRegion(apiKey)
+     fun getRegionsHttp() : RetrofitClient.Companion {
+        RetrofitClient.getInstance().getApi()
+            ?.getRegions(weatherAPI)
             ?.enqueue(object : retrofit2.Callback<ResponseBody> {
                 override fun onResponse(
                     call: Call<ResponseBody>,
                     response: Response<ResponseBody>
                 ) {
                     if (response.isSuccessful) {
-                        var dataBody = response.body()?.string()
-                        Toast.makeText(this@MainActivity, dataBody, Toast.LENGTH_SHORT).show()
+                        val successFul = response.body()?.string()
+                        Toast.makeText(this@MainActivity, successFul, Toast.LENGTH_SHORT).show()
+
                     } else {
                         try {
-                            val error = response.errorBody()?.string()
-                            var catchError = error
+                            val errorDetect = response.errorBody()?.string()
+                            val error = errorDetect
                             Toast.makeText(this@MainActivity, error, Toast.LENGTH_SHORT).show()
 
                         } catch (e: Exception) {
-                            e.printStackTrace()
-                            Toast.makeText(
-                                this@MainActivity,
-                                "something went wrong",
-                                Toast.LENGTH_SHORT
-                            ).show()
+                            Toast.makeText(this@MainActivity, "something wrong", Toast.LENGTH_SHORT)
+                                .show()
+
                         }
                     }
                 }
@@ -48,7 +51,10 @@ class MainActivity : ComponentActivity() {
                     Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
                 }
             })
+return RetrofitClient
     }
+
+
 }
 
 
